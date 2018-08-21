a75d7e016217d01d8e5ef98c93e2a4906ada8f79
Spencer Richards <spencer.richards7@gmail.com>
2018-08-21 13:58:47 -0600

    added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+categories = ["Ruby", "JavaScript", "Testing", "SQL", "React", "API", "Deployment"]
+
+user = User.create(email: 'user@test.com', password: 'password')
+puts "User seeded, email: user@test.com, password: password."
+
+25.times do
+  opt1 = Faker::ParksAndRec.character
+  opt2 = Faker::ElderScrolls.city
+  opt3 = Faker::Cat.breed
+  opt4 = Faker::SiliconValley.character
+  titles = [opt1, opt2, opt3, opt4]
+  
+  user.companies.create(
+    title: titles.sample,
+    location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}"
+  )
+end
+puts "25 companies seeded."
+
+# 50.times do
+#   opt1 = Faker::ParksAndRec.character
+#   opt2 = Faker::ElderScrolls.city
+#   opt3 = Faker::Cat.breed
+#   opt4 = Faker::SiliconValley.character
+#   titles = [opt1, opt2, opt3, opt4]
+#   category = categories.sample
+
+#   user.topics.create(
+#     category: category, 
+#     title: titles.sample, 
+#     body: "<h1>#{category} Content Goes Here</h1><p><br></p><ul><li>point one</li><li>point two</li><li>point three</li></ul><p><br></p><br><h3>#{Faker::MichaelScott.quote}</h3>"
+#   )
+# end
+
+# puts "50 topics seeded."
